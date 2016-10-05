1c8ce79d6dcf6158db288f7674e177faeac89889
zhhjemotion <zhhjemotoin@hotmail.com>
2016-10-05 11:32:08 +0200

    ZHHJemotion opencv_test2

---
+++ opencv_test2/opencv_test2/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  opencv_test2
+//
+//  Created by 张行剑 on 2016/10/5.
+//  Copyright © 2016年 Lukas_Zhang. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
