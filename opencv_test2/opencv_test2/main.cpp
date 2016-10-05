839543260d7b0ffb65f4e37f3523220fe41211ef
zhhjemotion <zhhjemotoin@hotmail.com>
2016-10-05 11:38:19 +0200

    opencv_test2

--- opencv_test2/opencv_test2/main.cpp
+++ opencv_test2/opencv_test2/main.cpp
@@ -7,9 +7,27 @@
 //
 
 #include <iostream>
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+using namespace cv;
+using namespace std;
 
 int main(int argc, const char * argv[]) {
     // insert code here...
     std::cout << "Hello, World!\n";
+    
+    VideoCapture cap(0);
+    if(!cap.isOpened()){
+        return -1;
+    }
+    Mat frame;
+    
+    while(true){
+        cap>>frame;
+        imshow("当前视频", frame);
+        if(waitKey(30) >= 0) break;
+    }
+    
     return 0;
 }
